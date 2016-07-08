88bbe67cd3992a8007c89c7fc57410dedee00ada
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2016-07-08 16:45:02 +0200

    add colax monoidal functor type

--- src/main/scala/monoidalFunctors.scala
+++ src/main/scala/monoidalFunctors.scala
@@ -48,3 +48,52 @@ extends AnyLaxMonoidalFunctor {
   type Functor = Functor0
   type TargetM = TM
 }
+
+trait AnyColaxMonoidalFunctor extends AnyFunctor {
+
+  type TargetM <: AnyMonoidalStructure
+  val sourceM: SourceM
+  type SourceM <: AnyMonoidalStructure
+  val targetM: TargetM
+
+  // NOTE notation
+  type □[X <: Source#Objects, Y <: Source#Objects] = SourceM# ⊗[X,Y]
+  type ⋄[X <: Target#Objects, Y <: Target#Objects] = TargetM# ⊗[X,Y]
+
+  type Source = SourceM#On
+  lazy val source = sourceM.on
+  type Target = TargetM#On
+  lazy val target = targetM.on
+
+  type Functor <: AnyFunctor {
+    type Source = SourceM#On
+    type Target = TargetM#On
+  }
+  val functor: Functor
+
+  type F[X <: Source#Objects] = Functor#F[X]
+
+  def apply[A <: Source#Objects, B <: Source#Objects](f: Source#C[A,B]): Target#C[F[A], F[B]] =
+    AnyFunctor.is(functor)(f)
+
+  def unzip[A <: Source#Objects, B <: Source#Objects]: Target#C[F[A □ B], F[A] ⋄ F[B]]
+
+  def counit: Target#C[F[SourceM#I], TargetM#I]
+}
+
+abstract class ColaxMonoidalFunctor[
+  SM <: AnyMonoidalStructure,
+  Functor0 <: AnyFunctor { type Source = SM#On; type Target = TM#On },
+  TM <: AnyMonoidalStructure
+]
+(
+  val sourceM: SM,
+  val functor: Functor0,
+  val targetM: TM
+)
+extends AnyColaxMonoidalFunctor {
+
+  type SourceM = SM
+  type Functor = Functor0
+  type TargetM = TM
+}
