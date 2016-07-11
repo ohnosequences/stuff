cdbb0e0b0469aa95350d1ff654089c0d2c27b4a9
Alexey Alekhin <laughedelic@gmail.com>
2016-07-11 16:19:27 +0200

    Trying to simplify spans; still no success

--- src/test/scala/spans.scala
+++ src/test/scala/spans.scala
@@ -3,37 +3,37 @@ package ohnosequences.stuff.test
 import org.scalatest.FunSuite
 import ohnosequences.stuff._
 
-trait AnySpan {
-
-  type Struct <: AnyCartesianMonoidalStructure with AnySymmetricMonoidalStructure
-  val  struct: Struct
-
-  type Cat = Struct#On
-  val  cat: Cat = struct.on
-
-  type Source <: Cat#Objects
-  type Target <: Cat#Objects
-
-  // type Top = Struct# ×[Source, Target]
-
-  val source: Cat#C[Struct# ×[Source, Target], Source]
-  val target: Cat#C[Struct# ×[Source, Target], Target]
-}
-
-case class Span[
-  CS <: AnyCartesianMonoidalStructure,
-  Source_ <: CS#On#Objects,
-  Target_ <: CS#On#Objects
-](val struct: CS)(
-  val source: CS#On#C[CS# ×[Source_, Target_], Source_],
-  val target: CS#On#C[CS# ×[Source_, Target_], Target_]
-) extends AnySpan {
-
-  type Struct = CS
-
-  type Source = Source_
-  type Target = Target_
-}
+// trait AnySpan {
+//
+//   type Struct <: AnyCartesianMonoidalStructure with AnySymmetricMonoidalStructure
+//   val  struct: Struct
+//
+//   type Cat = Struct#On
+//   val  cat: Cat = struct.on
+//
+//   type Source <: Cat#Objects
+//   type Target <: Cat#Objects
+//
+//   // type Top = Struct# ×[Source, Target]
+//
+//   val source: Cat#C[Struct# ×[Source, Target], Source]
+//   val target: Cat#C[Struct# ×[Source, Target], Target]
+// }
+//
+// case class Span[
+//   CS <: AnyCartesianMonoidalStructure,
+//   Source_ <: CS#On#Objects,
+//   Target_ <: CS#On#Objects
+// ](val struct: CS)(
+//   val source: CS#On#C[CS# ×[Source_, Target_], Source_],
+//   val target: CS#On#C[CS# ×[Source_, Target_], Target_]
+// ) extends AnySpan {
+//
+//   type Struct = CS
+//
+//   type Source = Source_
+//   type Target = Target_
+// }
 
 
 trait AnySpanCat extends AnyCategory { spn =>
@@ -42,19 +42,20 @@ trait AnySpanCat extends AnyCategory { spn =>
   val  struct: Struct
   lazy val struct_ = AnyMonoidalStructure.is(struct)
 
+  type × [A <: Struct#On#Objects, B <: Struct#On#Objects] = Struct# ×[A, B]
+
   type Cat = Struct#On
   val  cat: Cat = struct.on
 
   type Objects = Cat#Objects
 
-  type C[X, Y] = AnySpan {
-    type Struct = spn.Struct
-    type Source = X
-    type Target = Y
-  }
+  type C[S <: Cat#Objects, T <: Cat#Objects] = (
+    Cat#C[S × T, S], // source
+    Cat#C[S × T, T]  // target
+  )
 
 
-  def id[X <: Cat#Objects]: C[X,X] = Span(struct)(struct_.left[X, X], struct_.right[X, X])
+  def id[X <: Cat#Objects]: C[X,X] = (struct_.left[X, X], struct_.right[X, X])
 
   def compose[X <: Objects, Y <: Objects, Z <: Objects]: (C[Y,Z], C[X,Y]) => C[X,Z] = (g, f) => {
     // TODO: need pullbacks on Cat
@@ -62,32 +63,38 @@ trait AnySpanCat extends AnyCategory { spn =>
   }
 }
 
-case object AnySpanCat {
+case class SpanCat[S <: AnyCartesianMonoidalStructure with AnySymmetricMonoidalStructure](val struct: S)
+extends AnySpanCat { type Struct = S }
 
-  def dagger[Spn <: AnySpanCat](spn: Spn) = DaggerCategory(spn, SpanDaggerFunctor(spn))
-}
-
-case class SpanCat[Cat_ <: AnyCategory](val cat: Cat_)
-extends AnySpanCat { type Cat = Cat_ }
+// case object AnySpanCat {
+//
+//   def dagger[Spn <: AnySpanCat](spn: Spn) = DaggerCategory(spn, SpanDaggerFunctor(spn))
+// }
 
 
 case class SpanDaggerFunctor[Spn <: AnySpanCat](val spn: Spn)
 extends DaggerFunctor[Spn](spn) {
-  type Struct = Spn#Struct
-  val  struct = spn.struct
-  val  struct_ = spn.struct_
+  // type Struct = Spn#Struct
+  // val  struct = spn.struct
+  // val  struct_ = spn.struct_
 
-  val spn_ = AnyCategory.is(spn)
+  // val spn_ = AnyCategory.is(spn)
 
-  def apply[X <: Spn#Objects, Y <: Spn#Objects](f: Spn#C[X,Y]): Spn#C[Y,X] =
-    Span(struct)(
-      spn.compose(
-        f.target: Struct#On#C[Struct# ×[X,Y], Y],
-        spn.struct.symmetry[Y, X]
-      ),
-      spn.compose(
-        f.source: Struct#On#C[Struct# ×[X,Y], X],
-        spn.struct.symmetry[Y, X]
+  def apply[X <: Spn#Cat#Objects, Y <: Spn#Cat#Objects](f: Spn#C[X,Y]): Spn#C[Y,X] = {
+    val (fsource, ftarget): (spn.cat.C[spn.×[X,Y], X], spn.cat.C[spn.×[X,Y], Y]) = f
+
+    val src: Spn#Cat#C[Spn# ×[Y,X], Y] =
+      spn.cat.compose(
+        ftarget,
+        spn.struct.symmetry[Y,X]
       )
-    )
+
+    val tgt: Spn#Cat#C[Spn# ×[Y,X], X] =
+      spn.cat.compose(
+        fsource,
+        spn.struct.symmetry[Y,X]
+      )
+
+    (src, tgt)
+  }
 }
